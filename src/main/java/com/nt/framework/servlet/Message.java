d47380d6855e9b2410b3bde252c8931f242141f9
jzp19920204 <412743165@qq.com>
2017-09-07 14:08:09 +0800

    修改

---
+++ src/main/java/com/nt/framework/servlet/Message.java
@@ -0,0 +1,15 @@
+package com.nt.framework.servlet;
+
+import java.io.Serializable;
+
+/**
+ * 
+ * @ClassName: Message
+ * @Description: 消息处理类
+ * @author: jinzhaopo
+ * @version: V1.0
+ * @date: 2017年9月7日 下午1:53:59
+ */
+public class Message implements Serializable {
+
+}
